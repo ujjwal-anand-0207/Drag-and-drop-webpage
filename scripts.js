007af021ca18c8e8235f8e7bc14737836a78adcc
ujjwal-anand-0207 <ujjwalsaxena0207@gmail.com>
2023-06-10 15:50:07 +0530

    final commit for drag and drop webpage

--- scripts.js
+++ scripts.js
@@ -3,27 +3,27 @@ function allowDrop(event) {
   }
   
   function drag(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dataTransfer.setData("text", event.target.id);
   }
   
   function drop(event) {
     event.preventDefault();
-    const itemId = event.dataTransfer.getData("text/plain");
+    const itemId = event.dataTransfer.getData("text");
     const item = document.getElementById(itemId);
     event.target.appendChild(item);
-    event.target.querySelector('.drop-message').style.display = 'none';
+    // event.target.querySelector('.drop-message').style.display = 'none';
     alert('Item dropped successfully!');
   }
   
   function resetContainers() {
     const sourceContainer = document.querySelector('.source-container');
     const destinationContainer = document.querySelector('.destination-container');
-  
+    
+    // Reset the source container by appending the items back
+    const items = destinationContainer.querySelectorAll('.item');
     // Reset the destination container
     destinationContainer.innerHTML = '<div class="drop-message">Drop items here</div>';
   
-    // Reset the source container by appending the items back
-    const items = sourceContainer.querySelectorAll('.item');
     items.forEach((item) => {
       sourceContainer.appendChild(item);
     });
