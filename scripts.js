c9143afe5d0b35880bb481d44f5d05764826d89b
ujjwal-anand-0207 <ujjwalsaxena0207@gmail.com>
2023-06-10 13:49:55 +0530

    commiting web page

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drag(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    const itemId = event.dataTransfer.getData("text/plain");
+    const item = document.getElementById(itemId);
+    event.target.appendChild(item);
+    event.target.querySelector('.drop-message').style.display = 'none';
+    alert('Item dropped successfully!');
+  }
+  
+  function resetContainers() {
+    const sourceContainer = document.querySelector('.source-container');
+    const destinationContainer = document.querySelector('.destination-container');
+  
+    // Reset the destination container
+    destinationContainer.innerHTML = '<div class="drop-message">Drop items here</div>';
+  
+    // Reset the source container by appending the items back
+    const items = sourceContainer.querySelectorAll('.item');
+    items.forEach((item) => {
+      sourceContainer.appendChild(item);
+    });
+  }
+  
